03ad12e0ae3455e553ca2e5bbae3b58cbe7ba709
Denilson Nascimento <denilsonn7187@gmail.com>
2024-09-21 17:02:25 -0300

    criando controller para criar lista de conteudos

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import { createUser, loginUser, getDataFromUser } from './controllers/UserController.js';
 import { checkToken } from './controllers/authController.js';
-
-
+import { insertContent } from './controllers/contentController.js';
 
 const app = express();
 app.use(express.json());
@@ -17,6 +16,9 @@ app.post('/myfavcontent/login', loginUser);
 
 //Private Route
 app.get('/myfavcontent/user/:id', checkToken, getDataFromUser);
+
+//Insert content
+app.post('/myfavcontent/user/:id/insertContent', checkToken, insertContent);
 
 
 
---
+++ controllers/contentController.js
@@ -0,0 +1,5 @@
+import connection from "../database/conexao";
+
+export async function insertContent(req,res) {
+    
+};
